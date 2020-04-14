91bb0c8c667be033ca0598c5971ae521504879d9
Yamboy1 <yamboyd1@gmail.com>
2020-04-14 21:50:58 +1200

    Install fcm

--- android/app/src/main/kotlin/com/ferox/intouch/MainActivity.kt
+++ android/app/src/main/kotlin/com/ferox/intouch/MainActivity.kt
@@ -5,8 +5,11 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+
+
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
+    //   override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    //       GeneratedPluginRegistrant.registerWith(flutterEngine);
+    //
+    //   }
 }
