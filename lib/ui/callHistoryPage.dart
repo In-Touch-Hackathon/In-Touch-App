278729593f6dbbc3814e50badf6f0932c1a91d97
Frostrix <frostrixz@gmail.com>
2020-04-16 01:56:47 +1200

    Created Info Page

---
+++ lib/ui/callHistoryPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CallHistoryScreen extends StatefulWidget {
+  @override
+  _CallHistoryScreenState createState() => _CallHistoryScreenState();
+}
+
+class _CallHistoryScreenState extends State<CallHistoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
